462bfa6e0de000df6b83c4ae6d173cf67f287f10
oliveirasantos7 <santosoliveira482@gmail.com>
2024-07-18 19:50:11 -0300

    Limpando rotas e arquivos desnecessarios

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+
+defineProps({
+    
+});
+</script>
+
+<template>
+    <Head title="Social media website" />
+
+Test
+</template>
+
+<style>
+
+</style>
